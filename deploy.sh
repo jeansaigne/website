0b2cc293d782dc55bc41bb57ca70208dfa47ed46
Aymeric Tibere <aymerictibere@gmail.com>
2019-04-14 16:34:51 +0200

    chore: passing deploy script to bash

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+if [ "$TRAVIS_BRANCH" != "master" ]; then
   echo "We're not on the master branch."
   # analyze current branch and react accordingly
   exit 0
