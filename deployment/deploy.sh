2d1a20edda68f9a62e92cb870b51d5ba9d3b4d4c
Aymeric Tibere <aymerictibere@gmail.com>
2019-04-16 23:58:23 +0200

    tesst: dumb change for auto deploy by bash script 03

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Deploying to docker hub..."
-docker build . -t jeansaigne/website:latest && \
-echo "$REGISTRY_PASSWORD" | docker login -u "$REGISTRY_USERNAME" --password-stdin && \
+docker build . -t jeansaigne/website:latest
+echo "$REGISTRY_PASSWORD" | docker login -u "$REGISTRY_USERNAME" --password-stdin
 docker push jeansaigne/website:latest
