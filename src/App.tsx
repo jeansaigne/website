99a5e404393103e15995b28e7ecc35ae9a88e2d3
Aymeric Tibere <jeansaigne@users.noreply.github.com>
2019-04-17 00:19:35 +0200

    Feat/continuous deployment config (#3)
    * chore: bullshit
    
    * chore: added deploy after_success
    
    * Make deploy.sh executable
    
    * chore: passing deploy script to bash
    
    * Make deploy.sh executable
    
    * chore: passing deploy script to bash
    
    * chore: passing deploy script to bash
    
    * chore: passing deploy script to bash
    
    * chore: passing deploy script to bash
    
    * tesst: dumb change for auto dpeloy
    
    * tesst: dumb change for auto deploy, final check !
    
    * tesst: dumb change for auto deploy, final check  1!
    
    * tesst: dumb change for auto deploy by bash script 01
    
    * tesst: dumb change for auto deploy by bash script 02
    
    * tesst: dumb change for auto deploy by bash script 03
    
    * tesst: dumb change for auto deploy by bash script 04
    
    * tesst: dumb change for auto deploy by bash script final check
    
    * tesst: dumb change for auto deploy by bash script final check 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+	        <ul>
+		        <li>Automagic done !</li>
+	        </ul>
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
