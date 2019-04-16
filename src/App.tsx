7085edf9d04eeee7a3bc6352150f0e7b9bdaa743
Aymeric Tibere <aymerictibere@gmail.com>
2019-04-16 22:47:51 +0200

    tesst: dumb change for auto deploy, final check  1!

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Dockerized & automagically deployed, thx to Watchtower !</h1>
+	        <ul>
+		        <li>Dockerized</li>
+		        <li>Watchtower on</li>
+		        <li>Partial automagic deploy</li>
+	        </ul>
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
